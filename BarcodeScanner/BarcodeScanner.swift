6074fa46c81af467819107278854d61e5fd8c695
Michał Moskała <moskala.michal@gmail.com>
2018-11-03 13:21:44 +0100

    Created project using Xcode template

---
+++ BarcodeScanner/BarcodeScanner.swift
@@ -0,0 +1,11 @@
+//
+//  BarcodeScanner.swift
+//  BarcodeScanner
+//
+//  Created by Michal Moskala on 03/11/2018.
+//  Copyright © 2018 Michal Moskala. All rights reserved.
+//
+
+class BarcodeScanner {
+
+}
